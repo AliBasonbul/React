8da899f0524d71b930cb80ebced15ec3b0acc114
AliBasonbul <alimohmmedbasunbul@gmail.com>
2025-01-08 13:03:19 +0300

    handle Data by Json

--- src/Home.js
+++ src/Home.js
@@ -1,22 +1,27 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import BlogList from "./BlogList";
 
 const Home = () => {
-    const [blogs, setBlogs] = useState([
-        { title: 'My new website', body: 'lorem ipsum...', author: 'Omar', id: 1 },
-        { title: 'Welcome party!', body: 'lorem ipsum...', author: 'Ali', id: 2 },
-        { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'Omar', id: 3 }
-    ])
-    const handleDelete = (id) => {
-        const newBlogs = blogs.filter((blog) => id !== blog.id);
-        setBlogs(newBlogs);
-    }
+    const [blogs, setBlogs] = useState(null)
+    const [isloading, setisloading] = useState(true);
+
+    useEffect(() => {
+        fetch("http://localhost:8000/blogs")
+            .then(res => {
+                return res.json()
+            })
+            .then((data) => {
+                setBlogs(data);
+                setisloading(false);
+            }
+            )
+
+    }, [])
 
     return (
         <div className="home">
-            <BlogList blogs={blogs} title="All the Bloges" handleDelete={handleDelete} />
-            <BlogList blogs={blogs.filter((blog) => blog.author === "Omar")} title="All the Bloges from Omar" />
-
+            {isloading && <div><h1>Loading...</h1></div>}
+            {blogs && <BlogList blogs={blogs} title="All the Bloges" />}
         </div>
     );
 }
